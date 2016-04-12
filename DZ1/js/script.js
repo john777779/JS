fb48ceaf64aa1cda6c8b76e979c97434d735bae8
Ievgen Shmulia <john77777@ukr.net>
2016-04-12 23:34:26 +0300

    fixed

--- DZ1/js/script.js
+++ DZ1/js/script.js
@@ -4,15 +4,29 @@ function pow(x, n) {
   for (var i = 1; i < n; i++) {
     result = result * x ;
   }
-
   return result;
 }
 
-var x = prompt('Введие число возводимое в степень');
-var n = prompt('Введите степень');
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
-if (n < 1) {
-  alert('Степень ' + n + ' Ведена ошибочная степень, введите целую степень, большую 0');
+var x = +prompt('Введие число возводимое в степень');
+alert (typeof x);
+var n = +prompt('Введите степень');
+alert (typeof n);
+
+isNumeric(x);
+isNumeric(n);
+
+
+if (isNaN(x) || isNaN(n)) {
+  alert ('Введены неверные значения');
+
 } else {
-  console.log( pow(x, n) );
+  if (n < 1) {
+    alert('Степень ' + n + ' Ведена ошибочная степень, введите целую степень, большую 0');
+  } else {
+    console.log( pow(x, n) );
+  }
 }
