968e4525a17cfb343b84ab300ef745f8dc4af9c7
Ievgen Shmulia <john77777@ukr.net>
2016-04-13 22:42:50 +0300

    fixed

--- DZ2/js/script.js
+++ DZ2/js/script.js
@@ -1,3 +1,8 @@
+
+function empty(data) {
+ return typeof data === 'undefined' || !data || data.length === 0 || data === '' ;
+}
+
 var arr = [];
 
 for (var i = 0; i < 5; i++) {
@@ -6,15 +11,16 @@ for (var i = 0; i < 5; i++) {
 
 
 var name = prompt('Введите имя');
+var k = false;
 
-if (name != '') {
+if (!empty(name)) {
   for (var i in arr) {
     if (arr[i]==name) {
       var k = true;
     }
   }
 
-  if (k === true) {
+if (k === true) {
   alert(name + ' ,вы успешно вошли');
   } else {
     alert('Имя не найдено');
